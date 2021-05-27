97db817622f47ed78ed6a62b277b79c1cc48e440
Iyxan23 <nurihsanalghifari@gmail.com>
2021-05-27 09:10:08 +0700

    fix: Fix wrong app location

--- streamdapp/apps.py
+++ streamdapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class StreamdConfig(AppConfig):
+class StreamdappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'streamd'
+    name = 'streamdapp'
