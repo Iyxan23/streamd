1d5fbb076d473b99fc517e187f21e2a847c3aa17
Iyxan23 <nurihsanalghifari@gmail.com>
2021-05-27 15:23:50 +0700

    feat: Partially implement homepage

--- streamdapp/models.py
+++ streamdapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Stream(models.Model):
+
+    stream_name = models.CharField(max_length=250)
+    username = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    stream_key = models.CharField(max_length=250)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.stream_name + " - " + self.username
