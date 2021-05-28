29110ea23888bedafafa81b865897379e08df359
Iyxan23 <nurihsanalghifari@gmail.com>
2021-05-28 10:14:50 +0700

    feat: Implement listing streams on homepage

--- streamdapp/views.py
+++ streamdapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Stream
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    streams = Stream.objects.all()
+    return render(request, "home.html", {"streams": streams})
