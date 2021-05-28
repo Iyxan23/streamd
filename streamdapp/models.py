d274e10ed93b1caae00f02a189eaf86f57c6d79b
Iyxan23 <nurihsanalghifari@gmail.com>
2021-05-28 09:58:26 +0700

    fix: Use User model instead of just username

--- streamdapp/models.py
+++ streamdapp/models.py
@@ -1,13 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Stream(models.Model):
 
     stream_name = models.CharField(max_length=250)
-    username = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=1000)
     stream_key = models.CharField(max_length=250)
     likes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.stream_name + " - " + self.username
+        return self.stream_name
